78db280c2c88b61c05adb601e98fc5c4ca1ffed8
konszczeb <konrad.szczeblewski@protonmail.com>
2023-10-01 11:29:35 +0200

    lab1

--- src/pl/lublin/wsei/java/cwiczenia/Pobieranietekstuzseparatorem.java
+++ src/pl/lublin/wsei/java/cwiczenia/Pobieranietekstuzseparatorem.java
@@ -2,18 +2,16 @@ package pl.lublin.wsei.java.cwiczenia;
 
 import java.util.Scanner;
 
-public class Pobieranietekstu {
+public class Pobieranietekstuzseparatorem {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         do {
             int num1=0, num2=0;
-            System.out.print("Podaj pierwszą liczbe: ");
+            System.out.print("Podaj liczby, które mam dodać: ");
             num1 = input.nextInt();
-            if (num1 == 0) break;
-                System.out.print("Podaj drugą liczbę: ");
             num2 = input.nextInt();
-            if (num2 ==0) break;
+            if ((num1 == 0) || (num2== 0)) break;
             System.out.printf("Wynik dodawania %d + %d = %d%n", num1, num2, num1 + num2);
         } while (true);
     }
